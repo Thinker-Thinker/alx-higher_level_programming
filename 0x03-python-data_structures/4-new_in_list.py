373fafb48b5677d1aecd4c8e7974cfdace7fe4d7
Thinker <alphacentuarian@gmail.com>
2023-10-09 23:01:50 +0100

    List interartion starts from i = 1

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    new_list = my_list
-    if (idx >= 0) and (idx < len(my_list)):
+def new_in_list(my_list, idx, element):
+    new_list = []
+    for i in my_list:
+        new_list.append(my_list[i - 1])
+    if (idx >= 0) and (idx < len(new_list)):
         new_list[idx] = element
         return new_list
     else:
