627bb07c4ec6fe8c35479394c0caf99292e49873
Thinker <alphacentuarian@gmail.com>
2023-10-14 01:36:57 +0100

    typo

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,6 +2,6 @@
 def print_sorted_dictionary(a_dictionary):
     keys = list(a_dictionary.keys())
     keys.sort()
-    for ky in keys:
+    for key in keys:
         print("{}: {}".format(key, a_dictionary[key]))
 
