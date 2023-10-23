e9df3786a66520f0204c64e642ef04f73e31268b
Thinker <alphacentuarian@gmail.com>
2023-10-24 00:11:57 +0100

    I cant read properly

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,9 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(0, x):
             listp = listp + 1
-            print("{}".format(my_list[i]) end="")
+            print("{:d}".format(my_list[i]), end="")
+        print("")
         return listp
     except IndexError:
+        print("")
         return listp
